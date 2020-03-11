52137f00617dd0cae8c481bf47ce18047117bdb5
Youngmin Park <codepark2019@gmail.com>
2020-03-11 02:04:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  var array = [];
+  for(var i=0; i < dwarves.length; i++){
+        array.push(`${i+1}. ${dwarves[i]} `)
+  }
+    return array.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var uppercasePlaneteerCalls = [];
+  for(var i=0; i < planeteerCalls.length; i++){
+    let exclamation = "!"
+    uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${exclamation}`.toUpperCase())
+  }
+  return uppercasePlaneteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for(var i=0; i < words.length; i++){
+    if(words.length > 4){
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  for(var i=0; i < foods.length; i++){
+    if(foods[i] === 'cheddar'){
+      return foods[i]
+    }
+  }
+      return 'no cheese!'
 }
